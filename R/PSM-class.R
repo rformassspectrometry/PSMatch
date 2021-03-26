4dc81184e8afa361b1d69d9a12a6b3e551899edc
lgatto <laurent.gatto@gmail.com>
2021-03-26 22:14:04 +0100

    reduced setter and getter

--- R/PSM-class.R
+++ R/PSM-class.R
@@ -13,25 +13,31 @@
 ##'
 ##' There are two types of `PSM` objects:
 ##'
-##' - Objects where each PSM is stored in a single data row. This also
-##'   holds for multiple matches to the same spectrum, be it different
-##'   peptide sequences or the same sequence with or without a
-##'   post-translational modification. In such cases, the spectrum
-##'   identifier column contains duplicated entries. Such objects are
-##'   typically created with the `PSM()` constructor starting from
-##'   `mzIdentML` files.
+##' - Objects with duplicated spectrum identifiers. This holds for
+##'   multiple matches to the same spectrum, be it different peptide
+##'   sequences or the same sequence with or without a
+##'   post-translational modification. Such objects are typically
+##'   created with the `PSM()` constructor starting from `mzIdentML`
+##'   files.
 ##'
-##' - Reduced object where the spectrum identifiers are unique keys
-##'   within the PSM table. Matches to the same scan/spectrum are
-##'   merged into a single data row. Reduced `PSM` object are created
-##'   with the `reducePSMs()` function. See examples below.
+##' - Reduced object where the spectrum identifiers (or any equivalent
+##'   column - see example below) are unique keys within the PSM
+##'   table. Matches to the same scan/spectrum in each raw data file
+##'   are merged into a single data row. Reduced `PSM` object are
+##'   created with the `reducePSMs()` function. See examples below.
+##'
+##' Objects can be checked for their reduced state with the
+##' `reduced()` function which returns TRUE for reduced instances,
+##' FALSE when the spectrum identifiers are duplicated, or NA when
+##' unknown. The flag can also be set explicitly with the
+##' `reduced()<-` setter.
 ##'
 ##' The constructor uses parsers provided by the `mzR` or `mzID`
 ##' packages to read the `mzIdentML` data. See the vignette for some
 ##' apparent differences in their outputs.
 ##'
-##'
 ##' @examples
+##'
 ##' f <- msdata::ident(full.names = TRUE, pattern = "TMT")
 ##' basename(f)
 ##'
@@ -45,14 +51,17 @@
 ##' i <- which(duplicated(psm$spectrumID))[1:2]
 ##' i <- which(psm$spectrumID %in% psm$spectrumID[i])
 ##' psm2 <- psm[i, ]
+##' reduced(psm2)
 ##'
 ##' ## Peptide sequence CIDRARHVEVQIFGDGKGRVVALGERDCSLQRR with
 ##' ## Carbamidomethyl modifications at positions 1 and 28.
 ##' DataFrame(psm2[, c("sequence", "spectrumID", "modName", "modLocation")])
+##' reduced(psm2) <- FALSE
 ##'
 ##' rpsm2 <- reducePSMs(psm2, psm2$spectrumID)
 ##' rpsm2
 ##' DataFrame(rpsm2[, c("sequence", "spectrumID", "modName", "modLocation")])
+##' reduced(rpsm2)
 NULL
 
 
@@ -71,8 +80,7 @@ setClass("PSM",
 setMethod("show", "PSM",
           function(object) {
               cl <- classNameForDisplay(object)
-              if (!is.null(metadata(object)[["reduced"]]) &&
-                  metadata(object)[["reduced"]])
+              if (isTRUE(reduced(object)))
                   cl <- paste("Reduced", cl)
               cat(cl, "with", nrow(object), "rows and",
                   ncol(object), "columns.\n")
@@ -109,11 +117,40 @@ setMethod("show", "PSM",
 ##'
 ##' @name PSM
 PSM <- function(files,
-                     parser = c("mzR", "mzID"),
-                     BPPARAM = SerialParam()) {
+                parser = c("mzR", "mzID"),
+                BPPARAM = SerialParam()) {
     if (!all(flex <- file.exists(files)))
         stop(paste(files[!flex], collapse = ", "), " not found.")
     parser <- match.arg(parser)
     if (parser == "mzR") readPSMsMzR(files, BPPARAM)
     else readPSMsMzID(files, BPPARAM)
+}
+
+##' @param spectrumID `character(1)` with the column name referring to
+##'     the spectrum identifier or any other unique key for the `PSM`
+##'     object `x`.
+##'
+##' @param x An instance of class `PSM`.
+##'
+##' @name PSM
+##'
+##' @export
+reduced <- function(x, spectrumID) {
+    if (!missing(spectrumID))
+        return(!is.list(x[[spectrumID]]) & !anyDuplicated(x[[spectrumID]]))
+    if (is.null(metadata(x)[["reduced"]]))
+        return(NA)
+    else metadata(x)[["reduced"]]
+}
+
+##' @param value `logical(1)` to set the reduced state of the `PSM`
+##'     object.
+##'
+##' @name PSM
+##'
+##' @export
+"reduced<-" <- function(x, value) {
+    stopifnot(is.logical(value))
+    metadata(x)[["reduced"]] <- value
+    x
 }
