b250362b27fe654d1c53db49ee9424c3bd544cf2
lgatto <laurent.gatto@gmail.com>
2021-12-14 12:01:51 +0100

    test: use Matrix in testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@
 # revert this when that issue in R is fixed.
 Sys.setenv("R_TESTS" = "")
 
-adj <- sparseMatrix(
+adj <- Matrix::sparseMatrix(
     i = c(1, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8),
     j = c(1, 2, 3, 4, 3, 4, 5, 6, 6, 7, 7, 1),
     x = 1,
@@ -11,7 +11,7 @@ adj <- sparseMatrix(
         paste0("p", 1:8),
         paste0("P", 1:7)))
 
-cc <- sparseMatrix(
+cc <- Matrix::sparseMatrix(
     i = c(1, 2, 3, 3, 4, 4, 5, 5, 6, 6, 6, 7, 7),
     j = c(1, 2, 3, 4, 3, 4, 5, 6, 5, 6, 7, 6, 7),
     x = c(2, 1, 2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 2),
@@ -21,27 +21,31 @@ cc <- sparseMatrix(
 
 adjMatrices <-
     List(
-        sparseMatrix(i = c(1, 2), j = c(1, 1), x = 1,
-                     dimnames = list(c("p1", "p8"),
-                                     "P1")),
-        sparseMatrix(i = 1, j = 1, x = 1,
-                     dimnames = list("p2", "P2")),
-        sparseMatrix(i = c(1, 1, 2, 2),
-                     j = c(1, 2, 1, 2),
-                     x = 1,
-                     dimnames = list(
-                         c("p3", "p4"),
-                         c("P3", "P4"))),
-        sparseMatrix(i = c(1, 1, 2, 2, 3),
-                     j = c(1, 2, 2, 3, 3),
-                     x = 1,
-                     dimnames = list(
-                         c("p5", "p6", "p7"),
-                         c("P5", "P6", "P7"))))
+        Matrix::sparseMatrix(
+                    i = c(1, 2), j = c(1, 1), x = 1,
+                    dimnames = list(c("p1", "p8"),
+                                    "P1")),
+        Matrix::sparseMatrix(
+                    i = 1, j = 1, x = 1,
+                    dimnames = list("p2", "P2")),
+        Matrix::sparseMatrix(
+                    i = c(1, 1, 2, 2),
+                    j = c(1, 2, 1, 2),
+                    x = 1,
+                    dimnames = list(
+                        c("p3", "p4"),
+                        c("P3", "P4"))),
+        Matrix::sparseMatrix(
+                    i = c(1, 1, 2, 2, 3),
+                    j = c(1, 2, 2, 3, 3),
+                    x = 1,
+                    dimnames = list(
+                        c("p5", "p6", "p7"),
+                        c("P5", "P6", "P7"))))
 
-df <- data.frame(peptide = paste0("p", c(1, 8, 2, 3, 4, 3, 4, 5, 5, 6, 6, 7)),
-           protein = rep(colnames(adj), colSums(adj)))
-psmdf <- PSM(df, protein = "protein", peptide = "peptide")
+psm <- PMS(data.frame(peptide = paste0("p", c(1, 8, 2, 3, 4, 3, 4, 5, 5, 6, 6, 7)),
+                      protein = rep(colnames(adj), Matrix::colSums(adj))),
+           protein = "protein", peptide = "peptide")
 
 library("testthat")
 library("PSMatch")
