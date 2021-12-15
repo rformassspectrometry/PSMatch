08620c8206d0ec971d48c992ee9329e321ed6f2c
lgatto <laurent.gatto@gmail.com>
2021-12-15 07:35:09 +0100

    fix: layout in plotAdjacencyMatrix()

--- R/adjacencyMatrix-plot.R
+++ R/adjacencyMatrix-plot.R
@@ -8,9 +8,9 @@
 ##'
 ##' @param layout A graph layout, as defined in the `ipgraph`
 ##'     package. Default is [layout_as_bipartite()].
-plotAdjacencyMatrix <- function(x, layout = igraph::layout_as_bipartite) {
+plotAdjacencyMatrix <- function(x, layout = igraph::layout_nicely) {
     g <- graph_from_incidence_matrix(x)
     V(g)$color <- ifelse(names(V(g)) %in% colnames(x), "steelblue", "orange")
-    plot.igraph(g, layout = layout_nicely)
+    plot.igraph(g, layout = layout)
     invisible(g)
 }
