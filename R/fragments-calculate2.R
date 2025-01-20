b0421575011f5140cba43f5f5a4ef0d02a1ee5bd
Sebastian Gibb <mail@sebastiangibb.de>
2025-01-20 22:36:30 +0100

    fix: import combn from utils

--- R/fragments-calculate2.R
+++ R/fragments-calculate2.R
@@ -232,7 +232,9 @@ calculateFragments2 <- function(sequence,
 #' @param max_mods Numeric. Indicates how many modifications can be applied at once.
 #' 
 #' @return list with all possible combinations of modifications
-#' 
+#'
+#' @importFrom utils combn
+#'
 #' @noRd
 #' 
 #' @examples
