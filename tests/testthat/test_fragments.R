49207ba2b4dd68cff288ad47fdf25116a66d5501
GuillaumeDeflandre <potamoidef@gmail.com>
2026-02-19 11:16:53 +0100

    test: Fixed errors in tests for .parseModifiedSequence

--- tests/testthat/test_fragments.R
+++ tests/testthat/test_fragments.R
@@ -341,34 +341,18 @@ test_that("clean sequences works", {
 
 test_that("proforma delta masses without prefixes are supported", {
     expect_equal(
-        .parseModifiedSequence(
-            c("EMEVEESPEK", "EM[+15.995]EVEESPEK")
-        ),
-        list(
-            rep(0, 10),
-            c(0, 15.995, 0, 0, 0, 0, 0, 0, 0, 0)
-        )
+        .parseModifiedSequence("EM[+15.995]EVEESPEK"),
+        c(0, 15.995, 0, 0, 0, 0, 0, 0, 0, 0)
     )
     expect_equal(
-        .parseModifiedSequence(
-            c("EM[+15.9949]EVEES[+79.9663]PEK", "EM[+15.995]EVEES[-18.01]PEK")
-        ),
-        list(
-            c(0, 15.9949, 0, 0, 0, 0, 79.9663, 0, 0, 0),
-            c(0, 15.995, 0, 0, 0, 0, -18.01, 0, 0, 0)
-        )
+        .parseModifiedSequence("EM[+15.995]EVEES[-18.01]PEK"),
+        c(0, 15.995, 0, 0, 0, 0, -18.01, 0, 0, 0)
     )
 })
 
 test_that("proforma delta masses with prefixes are supported", {
     expect_equal(
-        .parseModifiedSequence(
-            c("EM[U:+15.9949]EVEES[M:+79.9663]PEK",
-              "EM[X:+15.995]EVEES[R:-18.01]PEK")
-        ),
-        list(
-            c(0, 15.9949, 0, 0, 0, 0, 79.9663, 0, 0, 0),
+        .parseModifiedSequence("EM[X:+15.995]EVEES[R:-18.01]PEK"),
             c(0, 15.995, 0, 0, 0, 0, -18.01, 0, 0, 0)
-        )
     )
 })
