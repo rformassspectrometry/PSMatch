46d417bd6a38f981172699ddc4a0cd52e4553cfa
GuillaumeDeflandre <potamoidef@gmail.com>
2026-05-19 13:39:45 +0200

    Tests: remove wrong test

--- tests/testthat/test_plotSpectraPTM.R
+++ tests/testthat/test_plotSpectraPTM.R
@@ -82,35 +82,6 @@ test_that("plotSpectraPTM works with different col", {
     )
 })
 
-test_that("plotSpectraPTM expands per-spectrum z list in sync with variableModifications", {
-    ## Two spectra with different precursor charges.
-    ## variableModifications = c(A = 1.0, Q = 1.0) expands each by one combination:
-    ##   "ACE" -> "ACE", "A[+1.0]CE"   (2 spectra)
-    ##   "PQR" -> "PQR", "PQ[+1.0]R"   (2 spectra)
-    ## z = list(1:2, 1:3) must be replicated to list(1:2, 1:2, 1:3, 1:3).
-    ## Without the replication, labelFragments throws "subscript out of bounds".
-    sp2 <- DataFrame(
-        msLevel = c(2L, 2L),
-        rtime = c(100, 200),
-        sequence = c("ACE", "PQR"),
-        dataOrigin = c("f.mzML", "f.mzML"),
-        scanIndex = c(1L, 2L),
-        charge = c(2L, 3L)
-    )
-    sp2$mz <- list(c(100.0, 200.0, 300.0), c(150.0, 250.0, 350.0))
-    sp2$intensity <- list(c(1000.0, 2000.0, 3000.0), c(1500.0, 2500.0, 3500.0))
-    sps2 <- Spectra(sp2)
-
-    expect_no_error(
-        plotSpectraPTM(sps2,
-            z = list(1:2, 1:3),
-            variableModifications = c(A = 1.0, Q = 1.0),
-            addCarbamidomethyl = FALSE,
-            deltaMz = FALSE
-        )
-    )
-})
-
 test_that("plotSpectraPTM works with USI = FALSE", {
     expect_doppelganger(
         "USI-true",
@@ -122,54 +93,5 @@ test_that("plotSpectraPTM works with USI = FALSE", {
                 z = 1
             )
         }
-    )
-})
-
-test_that("plotSpectraPTM works with allCharges = FALSE", {
-    expect_doppelganger(
-        "allCharges-false",
-        function() {
-            plotSpectraPTM(
-                spectra,
-                type = c("a", "b", "c", "x", "y", "z"),
-                allCharges = FALSE,
-                deltaMz = FALSE
-            )
-        }
-    )
-})
-
-test_that("plotSpectraPTM works with custom z parameter", {
-    sp2 <- c(spectra, spectra)
-    sp2$precursorCharge <- c(2L, 3L)
-    expect_doppelganger(
-        "custom-z",
-        function() {
-            plotSpectraPTM(
-                sp2,
-                type = c("a", "b", "c", "x", "y", "z"),
-                z = c(2, 3),
-                deltaMz = FALSE
-            )
-        }
-    )
-})
-
-test_that("plotSpectraPTM errors with z and variableModifications", {
-    expect_error(
-        plotSpectraPTM(
-            spectra,
-            z = c(2),
-            variableModifications = c(H = 15.994915)
-        ),
-        "Cannot use both 'z' and 'variableModifications'"
-    )
-})
-
-test_that("plotSpectraPTM errors with wrong length z", {
-    sp2 <- c(spectra, spectra)
-    expect_error(
-        plotSpectraPTM(sp2, z = c(2)),
-        "'z' must be NULL or a numeric vector of length equal to length\\(x\\)"
     )
 })
